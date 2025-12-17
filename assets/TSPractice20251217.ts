dde8ee00cfe5e20e7db55ab33d469b385119ebdb
WadeLiu <wadeliu@yile.com.tw>
2025-12-17 09:50:36 +0800

    練習泛型用法 及條件約束

---
+++ assets/TSPractice20251217.ts
@@ -0,0 +1,40 @@
+import {_decorator, Component} from 'cc';
+
+const {ccclass, property} = _decorator;
+
+@ccclass('TSPractice')
+export class TSPractice20251217 extends Component {
+    
+    
+    start(){
+        let arr = ['1,2,3'];
+        let numberbox = new Box<number>(9527);
+        let stringbox = new Box<string>('9527String');
+        console.log(stringbox);
+        console.log(numberbox);
+        console.log(arr);
+    }
+    wrapValue(value: string): string[] {
+        return [value];
+    }
+
+    wrapValue2<T>(value: T): T[] {
+        return [value];
+    }
+}
+
+class Box<T> {
+    private readonly _value: T;
+
+    constructor(value: T) {
+        this._value = value;
+    }
+
+    getValue(): T {
+        return this._value;
+    }
+}
+
+
+
+
